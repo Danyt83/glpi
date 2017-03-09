13a0ad9b580e12fc1c5db9908fa3f222b0e0c143
Danyt83 <dtraversa83@gmail.com>
2017-03-09 09:49:10 +0100

    Update index.php

--- index.php
+++ index.php
@@ -142,14 +142,7 @@ if (!file_exists(GLPI_CONFIG_DIR . "/config_db.php")) {
          <input type="submit" name="submit" value="'._sx('button', 'Post').'" class="submit" />
          </p>';
 
-   if ($CFG_GLPI["use_mailing"]
-       && countElementsInTable('glpi_notifications',
-                               "`itemtype`='User'
-                                AND `event`='passwordforget'
-                                AND `is_active`=1")) {
-      echo '<a id="forget" href="front/lostpassword.php?lostpassword=1">'.
-             __('Forgotten password?').'</a>';
-   }
+
    Html::closeForm();
 
    echo "<script type='text/javascript' >\n";
